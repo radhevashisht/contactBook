a09ddf5a9904ae470277eac0c358adad3c78c57c
radhevashist <radhevashist@example.com>
2025-06-21 18:00:15 +0530

    Configure API for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+// This is the URL for the local development server
 // export default axios.create({ baseURL: "http://localhost:3001" });
 
-// TODO: Replace <YOUR-GITHUB-USERNAME> and <YOUR-REPO-NAME> with your actual GitHub details
-export default axios.create({ baseURL: "http://localhost:3001" });
+// This is the URL for the deployed version, using My JSON Server
+export default axios.create({
+  baseURL: "https://my-json-server.typicode.com/radhevashisht/contactBook",
+});
